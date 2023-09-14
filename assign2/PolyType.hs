5352eef8405f4301644d6fb119c3edb472e459f6
Jan <rhbhendron@gmail.com>
2023-09-15 00:30:07 +0100

    Finished 2.4.2

--- assign2/PolyType.hs
+++ assign2/PolyType.hs
@@ -1,20 +1,26 @@
 module PolyType where
 
 --2.4.1 f8 can be used on arguments of type String
-f8 :: Ord a => a -> a -> a
 f8 x y  = if x <= y then x else y
 
-f9 :: Bool -> Bool -> Bool
 f9 x y  = not x || y
 
-f10 :: (Eq p, Num p) => p -> p -> p
 f10 x y
   | x == 0    = y
   | otherwise = x + y
 
 --2.4.1 f11 can be used on arguments of type String
-f11 :: p -> p -> p
 f11 x y = get 0
   where get n = if n == 0 then x else y
   
---2.4.2 TODO
+--2.4.2
+{-
+f8 is Ad-Hoc Polymorphic, as it only works on the typeclass Ord
+
+f9 is not polymorphic, as it only works on Booleans
+
+f10 is Ad-Hoc Polymorphic, as it works on only the typeclass Num
+
+f11 is Parametic Polymorphic, as it can return x or y, and there is no operations performed on either of them
+
+-}
