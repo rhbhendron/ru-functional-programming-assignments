25d40d6e9cf4082d6e9e9323024b8d22b6a369d6
shulscher <shulscher@science.ru.nl>
2023-09-14 12:23:08 +0200

    Completed exercise 2.5

--- assign2/Char.hs
+++ assign2/Char.hs
@@ -2,13 +2,28 @@ module Char where
 
 import Data.Char
 
---(~~) :: String -> String -> Bool
+--2.5.1
+(~~) :: String -> String -> Bool
+(~~) x y = map toLower x == map toLower y
 
---reverseCase :: String -> String
+--2.5.2
+reverseCase :: String -> String
+reverseCase x = map reverseCase' x
 
---shift :: Int -> Char -> Char
+reverseCase' :: Char -> Char
+reverseCase' x = if isLower x then toUpper x else toLower x
 
---caesar :: Int -> String -> String
+--2.5.3
+shift :: Int -> Char -> Char
+shift x y
+  | isUpper y = chr ((ord y - ord 'A' + x) `mod` 26 + ord 'A')
+  | otherwise = y
+  
+--2.5.4
+caesar :: Int -> String -> String
+caesar x y = map (shift x) $ map toUpper y
 
+--2.5.5
 msg :: String
 msg = "ADMNO D HPNO NKMDIFGZ TJP RDOC AVDMT YPNO"
+--"FIRST I MUST SPRINKLE YOU WITH FAIRY DUST"
