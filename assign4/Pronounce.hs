25c61b3f3c819941b67d1a492b402bfa19ab90ae
shulscher <shulscher@science.ru.nl>
2023-09-27 14:40:50 +0200

    Finished 4.7.1 up until 4.7.4

--- assign4/Pronounce.hs
+++ assign4/Pronounce.hs
@@ -11,3 +11,22 @@ instance Pronounceable Char where
 
 instance Pronounceable a => Pronounceable [a] where
   pronounce xs = "a list containing " ++ (intercalate ", " $ map pronounce xs)
+
+--4.7.1 and 4.7.2
+instance Pronounceable Integer where
+  pronounce :: Integer -> String
+  pronounce = say
+
+instance Pronounceable Int where
+  pronounce n = say (toInteger n)
+
+--4.7.3
+instance Pronounceable Double where
+  pronounce n =
+    let integerPart = truncate n
+        decimalPart = round $ 10 * (n - fromIntegral integerPart)
+    in say integerPart ++ " point " ++ say decimalPart
+
+--4.7.4
+instance (Pronounceable a, Pronounceable b) => Pronounceable (a, b) where
+  pronounce (a, b) = "a tuple containing " ++ pronounce a ++ " and " ++ pronounce b
